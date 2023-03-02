fa4d09b9843539567807341c785ea3bb5845a106
sabanonitsuke <sabakan.nari@gmail.com>
2023-03-02 12:26:03 +0900

    answersテーブル作成

--- db/migrate/20230302032418_create_answers.rb
+++ db/migrate/20230302032418_create_answers.rb
@@ -1,7 +1,9 @@
 class CreateAnswers < ActiveRecord::Migration[6.0]
   def change
     create_table :answers do |t|
-
+      t.integer :question_id
+      t.text :content
+      t.string :name
       t.timestamps
     end
   end
